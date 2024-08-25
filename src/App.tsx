dcecc1d5b9e9113a29aa6f85ca243cd76a429551
chie-hira <capella.a36@gmail.com>
2024-08-26 08:57:19 +0900

    ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import SignUp from "./components/SignUp";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import Login from "./components/Login";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <>
-            <Login />
-            <SignUp />
-            <Main />
-            <NotFound />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/" element={<Main />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
