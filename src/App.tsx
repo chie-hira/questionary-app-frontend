8d6d116749b0ba6aaeea28077a8e7e410ba7510a
chie-hira <capella.a36@gmail.com>
2024-08-26 09:30:28 +0900

    認証ルーティングをAppに適用

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import Login from "./components/Login";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {GuestRoute, PrivateRoute} from "./AuthRoute";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<GuestRoute children={<Login />} />} />
+                <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
+                <Route path="/" element={<PrivateRoute children={<Main />} />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
